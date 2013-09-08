8b1e34d87ac2faff8d192462d5e6eff2bba38da8
pi19404 <pi19404@gmail.com>
2013-09-08 20:45:31 +0530

    WEBSITE

--- _site/Algorithm/sort/rd.hs
+++
@@ -1,7 +0,0 @@
-main = do  
-        contents <- readFile "/tmp/a.txt"
-        print . map readInt . words $ contents
--- alternately, main = print . map readInt . words =<< readFile "test.txt"
-
-readInt :: String -> Int
-readInt = read
